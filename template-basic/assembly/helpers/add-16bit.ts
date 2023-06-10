da1352aff1309098e77fa6068aec3e226663fcd0
dipdowel <leonid.bogouslavski@gmail.com>
2023-06-10 21:16:47 +0200

    - basic example improved, no more 16bit addition

--- template-basic/assembly/helpers/add-16bit.ts
+++
@@ -1,3 +0,0 @@
-export function add16bit(a: i16, b: i16): i16 {
-  return a + b;
-}
--- template-basic/assembly/index.as.ts
+++ template-basic/assembly/index.as.ts
@@ -1,16 +1,18 @@
 // The entry point of your WebAssembly module.
 
-import { add16bit } from "./helpers/add-16bit";
+import { addTwoNumbers } from "./helpers/add-two-numbers";
 
 /**
- * This is a simple addition of two i32 values
+ * This is a simple addition of two i32 values.
+ * Under the hood the addition will be
  * @param a
  * @param b
  */
 export function add(a: i32, b: i32): i32 {
   console.log("[WASM] Hi from the WASM world!");
-  const result = add16bit(<i16>a, <i16>b);
-  // const t: i32;
+
+  // Call the function in the helper file
+  const result = addTwoNumbers(a, b);
 
   // AssemblyScript allows printing to the console of your browser
   console.log(`[WASM]    add(${a}, ${b}) --> ${result.toString(10)} `);
